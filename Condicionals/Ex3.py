72c20ccd0694e8c1c5351e6550c183e767dbe129
jaumepape <jaume@vdata.dev>
2025-07-17 12:50:52 +0200

    Ex3

---
+++ Condicionals/Ex3.py
@@ -0,0 +1,5 @@
+numero = int(input("Ingrese un numero: "))
+if numero >= 10 and numero <= 20:
+    print ("El numero está dentro del intervalo 10-20")
+else:
+    print ("El numero está fuera del intervalo 10-20")
