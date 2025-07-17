939b2821b092fa04194cd65b3771c5cf419fb5dc
jaumepape <jaume@vdata.dev>
2025-07-17 12:18:16 +0200

    frizzbuzz

--- frizzbuzz.py
+++ frizzbuzz.py
@@ -1,9 +1,9 @@
 for numero in range(1, 101):
-    if numero % 3 != 0 and numero % 5 != 0:
+    if numero % 3 == 0 and numero % 5 == 0:
         print ("frizzbuzz")
-    elif numero % 5 != 0:
+    elif numero % 5 == 0:
         print ("buzz")
-    elif numero % 3 != 0:
+    elif numero % 3 == 0:
         print ("firzz")
     else:
         print (numero)
