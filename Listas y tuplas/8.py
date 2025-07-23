5c210e8a988dc7c72158b512e65210bb67f372a6
jaumepape <jaume@vdata.dev>
2025-07-23 12:32:16 +0200

    Lista 7, 8

--- Listas y tuplas/8.py
+++ Listas y tuplas/8.py
@@ -1,9 +1,9 @@
-prices = [50, 75, 46, 22, 80, 65, 8]
-min = max = prices[0]
-for price in prices:
-    if price < min:
-        min = price
-    elif price > max:
-        max = price
-print("El mínimo es " + str(min))
-print("El máximo es " + str(max))
+word = input("Introduce una palabra: ")
+reversed_word = word
+word = list(word)
+reversed_word = list(reversed_word)
+reversed_word.reverse()
+if word == reversed_word:
+    print("Es un palíndromo")
+else:
+    print("No es un palíndromo")
